e618a0ecff2fd7161c69e6bd7d631a08638a4700
Filip Huhta <filip.huhta@consid.se>
2024-09-15 11:54:27 +0200

    formatted code and added required

--- src/pages/api/gemini.ts
+++ src/pages/api/gemini.ts
@@ -11,29 +11,43 @@ export default async function handler(
 ) {
   const API_KEY = process.env.API_KEY;
 
+  const schema = {
+    type: SchemaType.OBJECT,
+    properties: {
+      recipe_title: { type: SchemaType.STRING },
+      recipe_ingredients: {
+        type: SchemaType.ARRAY,
+        items: { type: SchemaType.STRING },
+      },
+      recipe_steps: {
+        type: SchemaType.ARRAY,
+        items: { type: SchemaType.STRING },
+      },
+      recipe_summary: { type: SchemaType.STRING },
+    },
+    required: [
+      "recipe_title",
+      "recipe_ingredients",
+      "recipe_steps",
+      "recipe_summary",
+    ],
+  };
+
   if (API_KEY) {
     const genAI = new GoogleGenerativeAI(API_KEY);
     const model = genAI.getGenerativeModel({
       model: "gemini-1.5-flash",
       generationConfig: {
         responseMimeType: "application/json",
-        responseSchema: {
-          type: SchemaType.OBJECT,
-          properties: {
-            recipe_title: { type: SchemaType.STRING },
-            recipe_ingredients: { type: SchemaType.ARRAY, items: { type: SchemaType.STRING } },
-            recipe_steps: { type: SchemaType.ARRAY, items: { type: SchemaType.STRING } },
-            recipe_summary: { type: SchemaType.STRING },
-          },
-        },
+        responseSchema: schema,
       },
     });
-    
+
     const preCondition = `
       I want you to come up with a recipe based on the user's input. If the user doesn't provide any input, 
       generate a random recipe. The response content should include all ingredients, steps, and a summary.  
       User's input for ingredients:`;
-    
+
     const prompt = `${preCondition} ${req.body}`;
 
     if (prompt) {
